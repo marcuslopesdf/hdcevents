5f4fcc7f62e1a8d77f960e2b741304088c0118fe
MarcusLopes <marcuslopesdf@yahoo.com.br>
2021-08-04 10:28:34 -0300

    Criação do dashboard do usuário

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -77,5 +77,14 @@ class EventController extends Controller
         return view('events.show', ['event'=>$event, 'eventOwner'=>$eventOwner]);
     }
 
+    public function dashboard(){
+
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        return view('events.dashboard', ['events'=>$ev  ents]);
+    }
+
 
 }
