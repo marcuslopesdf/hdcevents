ca30615a87024d907b765bada213115d98729af5
MarcusLopes <marcuslopesdf@yahoo.com.br>
2021-08-05 22:55:02 -0300

    Implementação da função de sair de um evento e restrição de múltiplas inscriçòes por um mesmo usuário

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -72,20 +72,32 @@ class EventController extends Controller
     {
 
         $event=Event::findOrFail($id);
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if($user){
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent){
+                if($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+        }
 
         $eventOwner =   User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event'=>$event, 'eventOwner'=>$eventOwner]);
+        return view('events.show', ['event'=>$event, 'eventOwner'=>$eventOwner, 'hasUserJoined'=>$hasUserJoined]);
     }
 
     public function dashboard()
     {
 
         $user = auth()->user();
-
         $events = $user->events;
+        $eventsAsParticipant = $user->eventsAsParticipant;
 
-        return view('events.dashboard', ['events'=>$events]);
+        return view('events.dashboard', ['events'=>$events, 'eventsasparticipant'=>$eventsAsParticipant]);
     }
 
     public function destroy($id)
@@ -134,6 +146,16 @@ class EventController extends Controller
         $event = Event::findOrFail($id);
 
         return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento '. $event->title);
+    }
+
+    public function leaveEvent($id){
+
+        $user = auth()->user();
+        $user->eventsAsParticipant()->detach($id);
+        $event = Event::findOrFail($id);
+        $hasUserJoined = false;
+
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento: '. $event->title);
     }
 
 
